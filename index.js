02ac846d975d6cd22677dac68b38471b1c5c2b9e
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-03-19 15:26:12 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+/*function superbowlWin(record){
+    if (record.result==="W") {
+    return record.year;
+}}*/
+
+function superbowlWin(record){
+    const winyear=record.find(game =>game.result ==='W');
+    if (winyear){
+        return winyear.year;
+    } else{
+        return undefined
+    }
+}
